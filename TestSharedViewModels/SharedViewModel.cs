348c8b574e559de60bea9ae8a697d65cf1b7288e
Omnironan <rshortall@omnifone.com>
2014-05-09 10:05:09 +0100

    Initial commit for MVVM Light test with PCL view model project

---
+++ TestSharedViewModels/SharedViewModel.cs
@@ -0,0 +1,30 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestSharedViewModels
+{
+    public class SharedViewModel : ViewModelBase
+    {
+        public SharedViewModel()
+        {
+            TestProperty = IsInDesignMode
+                ? "This Test Property from Design Mode"
+                : "This Test Property from Runtime Mode";
+        }
+
+        private string _testProperty;
+
+        public string TestProperty
+        {
+            get { return _testProperty; }
+            set
+            {
+                _testProperty = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
---
+++ TestUniversalMVVM/TestUniversalMVVM.Shared/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿//using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestUniversalMVVM.ViewModel
+{
+    //public class MainViewModel : ViewModelBase
+    //{
+    //    private string _helloWorld;
+
+    //    public string HelloWorld
+    //    {
+    //        get { return _helloWorld; }
+    //        set { Set(() => HelloWorld, ref _helloWorld, value); }
+    //    }
+
+    //    public MainViewModel()
+    //    {
+    //        HelloWorld = IsInDesignMode
+    //            ? "Runs in design mode"
+    //            : "Runs in runtime mode";
+    //    }
+    //}
+}
